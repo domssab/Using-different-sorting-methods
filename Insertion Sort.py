e7c23fbf85569ccd1d9c4545060a6936fcb3aebb
domssab <saballo.chantaldominique@gmail.com>
2023-01-27 19:07:42 +0800

    printing outputs

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -6,3 +6,10 @@ def InsertionSort(array):
         while j >= 0 and key < array[j]:
             array[j + 1] = array[j]
             j = j - 1
+
+        array[j + 1] = key
+
+data = [7, 19, 24, 11, 17, 29]
+InsertionSort(data)
+print("Sorted array in ascending order: ")
+print(data)
