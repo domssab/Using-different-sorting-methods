fc15dde5be5c7a3c9414d30987fa7394c5ff48d3
domssab <saballo.chantaldominique@gmail.com>
2023-01-27 17:38:57 +0800

    for array[i]:

--- Selection Sort.py
+++ Selection Sort.py
@@ -1,3 +1,8 @@
 def slectionSort(array, size):
     for step in range(size):
         min_idx = step
+
+        for i in range(step + 1, size):
+
+            if array[i] < array[min_idx]:
+                min_idx = i
