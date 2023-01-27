91a3f153acf5b36245503efb5da6f51de8fb1872
domssab <saballo.chantaldominique@gmail.com>
2023-01-27 19:30:25 +0800

    arrays

--- Quick Sort.py
+++ Quick Sort.py
@@ -6,3 +6,6 @@ def partition(array, low, high):
         if array[j] <= pivot:
             i = i + 1
             (array[i], array[j]) = (array[j], array[i])
+
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    return i + 1
