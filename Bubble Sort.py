d35a554e637bdcfed1819494eaba648e989478c0
domssab <saballo.chantaldominique@gmail.com>
2023-01-27 19:00:17 +0800

    printing outputs

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -6,3 +6,10 @@ def BubbleSort(array):
                 array[j] = array[j + 1]
                 array[j + 1] = temp
 
+data = [7, 19, 24, 11, 17, 29]
+
+BubbleSort(data)
+
+print("Sorted array in ascending order: ")
+print(data)
+
